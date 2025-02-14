9a085f24d2a6b5e4e4045f0e4dee5ad65d5cb089
Samir Gamal <samirgamal2023@gmail.com>
2025-02-15 00:29:35 +0200

    Problem Solving

---
+++ Data Type and Condition/Timon and Pumbaa.py
@@ -0,0 +1,9 @@
+# Title : Timon and Pumbaa
+# Link : https://codeforces.com/group/MWSDmqGsZm/contest/326907/problem/A
+inputs=input().split()
+a=int(inputs[0])
+b=int(inputs[1])
+if a>b:
+    print(a-b)
+else:
+    print(0)
